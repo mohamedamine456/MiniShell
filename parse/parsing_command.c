53d65634f959cda67e17763afaf27e0ea4f7ff7d
mohamedamine456 <aminelachheb456@gmail.com>
2021-03-29 17:08:15 +0100

    start cutting that thing

--- parse/parsing_command.c
+++ parse/parsing_command.c
@@ -6,7 +6,7 @@
 /*   By: mlachheb <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/29 15:04:10 by mlachheb          #+#    #+#             */
-/*   Updated: 2021/03/29 16:48:48 by mlachheb         ###   ########.fr       */
+/*   Updated: 2021/03/29 17:07:46 by mlachheb         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,17 +14,22 @@
 
 int		parse_command(char *line, char **envp)
 {
+	t_cmd		*cmd;
 	int			i;
-	t_escapes	escp;
 	int			n_s;		//next separator
+	char		*tmp_part;
 
 	i = 0;
 	n_s = 0;
-	escp = (t_escape){0, 0, 0};
+	//cmd = initialize_cmd();
 	if (line == NULL)
 		return (1);
 	while (str[i] != '\0')
 	{
-
+		n_s = next_separator(str + i);
+		tmp_part = ft_substr(str, i, n_s);
+		i += n_s;
+		//put_part_to_command(tmp_part, str[i], &cmd);
+		i++;
 	}
 }
