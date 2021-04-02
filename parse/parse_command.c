7095d2f02c2814e47f14a1f80200991735dc6399
ELORCHI <elorchie@gmail.com>
2021-04-02 19:54:13 +0100

    amine's code

--- parse/parse_command.c
+++ parse/parse_command.c
@@ -1,29 +1,28 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   parse_command.c                                    :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: mlachheb <marvin@42.fr>                    +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/03/29 17:59:18 by mlachheb          #+#    #+#             */
-/*   Updated: 2021/03/31 09:01:16 by mlachheb         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include "parse.h"
+
+char    **cut_line(char *line);
+char    *cut_separator(char *line, int *i);
 
 int		parse_command(char *line, char **envp)
 {
+	char **tab;
+	tab = cut_line(line);
+	tab = clean_tab_cmd(tab);
+	ft_print_args(tab);
+	ft_free_args(tab);
+	return (0);
+}
+char	**cut_line(char *line)
+{
 	int			i;
-	int			n_s;		//next separator
+	int			n_s;
 	char		*tmp_part;
 	char		**tab;
-
 	i = 0;
 	n_s = 0;
 	tab = NULL;
 	if (line == NULL)
-		return (1);
+		return (NULL);
 	while (line[i] != '\0')
 	{
 		n_s = ft_next_separator(line + i);
@@ -32,20 +31,38 @@ int		parse_command(char *line, char **envp)
 		if (ft_strcmp("", tmp_part))
 			tab = ft_resize_tab(tab, ft_strdup(tmp_part));
 		i += n_s;
-		if (ft_isseparator(line[i]))
-		{
-			if (!ft_isspace(line[i]))
-			{
-				free(tmp_part);
-				tmp_part = ft_substr(line, i, 1);
-				tab = ft_resize_tab(tab, ft_strdup(tmp_part));
-			}
-			i++;
-		}
+		free(tmp_part);
+		tmp_part = cut_separator(line, &i);
+		if (tmp_part != NULL)
+			tab = ft_resize_tab(tab, ft_strdup(tmp_part));
 		free(tmp_part);
 	}
-	tab = clean_tab_cmd(tab);
-	ft_print_args(tab);
-	ft_free_args(tab);
-	return (0);
+	return (tab);
+}
+char	*cut_separator(char *line, int *i)
+{
+	char	*tmp_part;
+	char	c;
+	int		j;
+	j = 0;
+	tmp_part = NULL;
+	if (ft_isseparator(line[*i]))
+	{
+		if (ft_isspace(line[*i]))
+			(*i)++;
+		else
+		{
+			if (line[*i] == '>' || line[*i] == '>')
+			{
+				c = line[*i];
+				while (line[*i + j] == c)
+					j++;
+				tmp_part = ft_substr(line, *i, j);
+				*i += j;
+			}
+			else
+				tmp_part = ft_substr(line, (*i)++, 1);
+		}
+	}
+	return (tmp_part);
 }
--- parse/replace_tab_env.c
+++ parse/replace_tab_env.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   replace_tab_env.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mlachheb <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: eel-orch <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/03/31 09:27:36 by mlachheb          #+#    #+#             */
-/*   Updated: 2021/03/31 09:48:19 by mlachheb         ###   ########.fr       */
+/*   Created: 2021/04/02 19:53:47 by eel-orch          #+#    #+#             */
+/*   Updated: 2021/04/02 19:53:54 by eel-orch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,23 +17,14 @@ char	**replace_tab_env(char **tab_cmd, char **envp)
 	int		i;
 
 	i = 0;
+	if (envp == NULL)
+		return (tab_cmd);
 	while (tab_cmd != NULL && tab_cmd[i] != NULL)
 	{
-		tab_cmd[i] = replace_str(tab_cmd[i], envp);
+		tab_cmd[i] = trim_replace(tab_cmd[i], envp);
 		i++;
 	}
 	return (tab_cmd);
-}
-
-char	*replace_str(char *str, char **envp)
-{
-	char	*new_str;
-
-	if (str == NULL || envp == NULL)
-		return (NULL);
-	new_str = trim_replace(str, envp);
-	free(str);
-	return (new_str);
 }
 
 char	*trim_replace(char *str, char **envp)
@@ -41,7 +32,6 @@ char	*trim_replace(char *str, char **envp)
 	int			i;
 	t_escapes	escp;
 
-	char *(*func[])(char *, char **, int *) = {rep_spec, rep_alpha, rep_digit};
 	i = 0;
 	escp = (t_escapes){0, 0, 0};
 	while (str[i] != '\0')
@@ -49,8 +39,10 @@ char	*trim_replace(char *str, char **envp)
 		escp = ft_check_escapes(escp, str[i]);
 		if (escp.s_q % 2 == 0 && escp.b_s == 0 && str[i] == '$')
 		{
-			if (ft_isalnum(str[i + 1]) || ft_char_in_string(str[i + 1], DLR_SPECIALS))
-				str = func[ft_char_to_func(str[i + 1])](str, envp, &i);
+			if (ft_isalpha(str[i + 1]) || str[i + 1] == '_')
+				str = replace_env(str, envp, &i);
+			else if (str[i + 1] == '?')
+				str = replace_return(str, &i);
 			else if (ft_char_in_string(str[i + 1], "\'\"") && escp.d_q % 2 == 0)
 			{
 				str = ft_strjoin(ft_substr(str, 0, i),
@@ -91,4 +83,38 @@ char	*search_env(char *str, char **envp)
 	}
 	free(str);
 	return (new_str);
+}
+
+char	*replace_env(char *str, char **envp, int *i)
+{
+	int		j;
+	char	*new_str;
+	char	*tmp;
+
+	j = 1;
+	while (str[*i + j] != '\0')
+	{
+		if (!ft_isalnum(str[*i + j]) && str[*i + j] != '_')
+			break ;
+		j++;
+	}
+	new_str = ft_substr(str, 0, *i);
+	tmp = ft_substr(str, *i + 1	, j);
+	tmp = search_env(tmp, envp);
+	if (tmp == NULL)
+		tmp = ft_strdup("");
+	new_str = ft_strjoin(new_str, tmp);
+	free(tmp);
+	tmp = ft_substr(str, *i + j + 1, ft_strlen(str) - *i - j - 1);
+	new_str = ft_strjoin(new_str, tmp);
+	free(str);
+	free(tmp);
+	return (new_str);
+}
+
+char	*replace_return(char *str, int *i)
+{
+	char *tmp_part;
+
+	return (str);
 }
