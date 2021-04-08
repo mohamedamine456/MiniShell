466570e0f4148a1be16e8c0777e72e9ebc5ab05f
mohamedamine456 <aminelachheb456@gmail.com>
2021-04-08 17:47:40 +0100

    open output files

--- open_files/open_files.h
+++ open_files/open_files.h
@@ -3,6 +3,8 @@
 # include "../libft/libft.h"
 # include "../parse/cmd.h"
 # include <errno.h>
+# include <unistd.h>
+# include <fcntl.h>
 
 int		open_inputs(t_input *inputs);
 
--- open_files/open_outputs.c
+++ open_files/open_outputs.c
@@ -1,40 +1,43 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   open_inputs.c                                      :+:      :+:    :+:   */
+/*   open_outputs.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mlachheb <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/04/08 10:44:11 by mlachheb          #+#    #+#             */
-/*   Updated: 2021/04/08 17:45:33 by mlachheb         ###   ########.fr       */
+/*   Created: 2021/04/08 17:25:29 by mlachheb          #+#    #+#             */
+/*   Updated: 2021/04/08 17:46:17 by mlachheb         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "open_files.h"
 
-int		open_inputs(t_input *inputs)
+int		open_outputs(t_output *outputs)
 {
 	int	new_fd;
 	int	tmp_fd;
-	int stdin_fd;
+	int	stdout_fd;
 
-	if (inputs == NULL)
+	if (outputs == NULL)
 		return (0);
-	stdin_fd = dup(0);
-	if (stdin_fd < 0)
+	stdout_fd = dup(1);
+	if (stdout_fd < 0)
 		return (-1);
-	while (inputs != NULL)
+	while (outputs != NULL)
 	{
-		new_fd = open(inputs->file, O_RDONLY, S_IRUSR);
+		if (outputs->type == SIMPLE_REDIRECTION)
+			new_fd = open(outputs->file, O_WRONLY, S_IWUSR);
+		else if (outputs->type == DOUBLE_REDIRECTION)
+			new_fd = open(outputs->file, O_WRONLY | O_APPEND, S_IWUSR);
 		if (new_fd < 0)
 			return (-1);
-		tmp_fd = dup2(new_fd, 0);
+		tmp_fd = dup2(new_fd, 1);
 		if (tmp_fd < 0)
 		{
 			close(new_fd);
 			return (-1);
 		}
-		inputs = inputs->next;
+		outputs = outputs->next;
 	}
 	return (0);
 }
