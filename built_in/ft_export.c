1d14690c82fb71ae1ae5d26623384f717ea0cfec
ELORCHI <elorchie@gmail.com>
2021-04-12 03:08:30 +0000

    add built in export not finished yet need sort string function

---
+++ built_in/ft_export.c
@@ -0,0 +1,53 @@
+#include "built_in.h"
+
+int	isvalid_var(char *str)
+{
+    int i;
+
+    if (ft_isdigit(str[0]) != 1 && str[0] != '_')
+	return (-1);
+    i = 1;
+    while (str[i] != '\0')
+    {
+	if (str[i] == '-' || str[i] == '*' || str[i] == '!')
+	    return (-1);
+	i++;
+    }
+    return (0);
+}
+
+char	*to_var(char *arg)
+{
+    int i;
+
+    i = ft_strstri((const char *)arg, "=");
+    if (i == -1)
+	return (ft_strjoin(ft_strdup(arg), "=''"));
+    return (arg);
+}
+
+void	ft_export(t_builtin_vars var, int *retv)
+{
+    char    **sorted_env = NULL;
+    int	    i;
+
+    i = 0;
+    if (var.args == NULL)
+    {
+	//sorted_env = sort_strings((const char *)var.envp);
+	ft_print_args(sorted_env);
+	*retv = 0;
+	//free sorted args
+    }
+    else
+    {
+	while (var.args[i] != NULL)
+	{
+	    if (isvalid_var(var.args[i]) == 0)
+		var.envp = ft_strjoin_args(var.envp, to_var(var.args[i]));
+	    else
+		*retv = -1;//invalide identifier
+	    i++;
+	}
+    }
+}
