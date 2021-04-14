e3b8c66dd76a5d8e5e32dabeefd858e25ab9fcea
mohamedamine456 <aminelachheb456@gmail.com>
2021-04-14 14:39:18 +0000

    forget an arg to open_outputs

--- open_files/open_in_out_files.c
+++ open_files/open_in_out_files.c
@@ -6,7 +6,7 @@
 /*   By: mlachheb <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/14 14:21:09 by mlachheb          #+#    #+#             */
-/*   Updated: 2021/04/14 14:30:14 by mlachheb         ###   ########.fr       */
+/*   Updated: 2021/04/14 14:38:35 by mlachheb         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 int		open_in_out_files(t_cmd *cmd)
 {
 	int		stdin_fd;
-	int		stout_fd;
+	int		stdout_fd;
 	int		retv;
 
 	if (cmd != NULL)
@@ -23,7 +23,7 @@ int		open_in_out_files(t_cmd *cmd)
 		retv = open_inputs(cmd->input, &stdin_fd);
 		if (retv == -1)
 			return (-1);
-		retv = open_outputs(cmd->output);
+		retv = open_outputs(cmd->output, &stdout_fd);
 		if (retv == -1)
 			return (-1);
 		return (0);
