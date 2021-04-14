c7e79994730d127d0492728b24aac30b4ed588a5
ELORCHI <elorchie@gmail.com>
2021-04-14 05:32:31 +0000

    fic leaks

--- libft/ft_jointabstr.c
+++ libft/ft_jointabstr.c
@@ -3,12 +3,15 @@
 
 char **ft_jointabstr(char **tab, char *str)
 {
-    char **tmp_tab;
-    char **new_tab;
+    char    **tmp_tab;
+    char    **new_tab;
+    int	    i;
 
     tmp_tab = (char **)malloc(sizeof(char *) * 2);
     tmp_tab[0] = ft_strdup(str);
     tmp_tab[1] = NULL;
     new_tab = ft_strjoin_tables(tab, tmp_tab);
+    free(tmp_tab[0]);
+    free(tmp_tab);
     return (new_tab);
 }
