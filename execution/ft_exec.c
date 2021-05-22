6477e07d28698780b53dde1be5b58c38d2df48e6
ELORCHI <elorchie@gmail.com>
2021-05-22 15:55:18 +0100

    redirections working with pipes

--- execution/ft_exec.c
+++ execution/ft_exec.c
@@ -5,10 +5,13 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 
+int test_file;
 
-int 	is_simple_cmd(t_cmd *cmd)
+void	print_error()
 {
-	return (isbuilt_in(cmd->name) && (size_cmds(cmd) == 1));
+
+	write(test_file, "error\n", 6);
+	exit(0);
 }
 
 int		**get_pipes(t_cmd *cmd)
@@ -29,26 +32,27 @@ int		**get_pipes(t_cmd *cmd)
 }
 
 // -1 is for errors handling
-int		open_inputs(t_input *inputs, int test)
+int		open_inputs(t_input *inputs, int fd)
 {
-	int		fd;
 	t_input *tmp;
 
 	tmp = inputs;
 	if (inputs == NULL)
 	{
+		write(fd, "no input files\n", ft_strlen("no input files\n"));
 		return (0);
 	}
 	while (tmp)
 	{
-		fd = open(tmp->file, O_RDONLY, 00400);
+		fd = open(tmp->file, O_RDWR |  O_APPEND | O_CREAT, 0777);;
 		if (fd == -1)
 		{
-			write(1, "error", 5);
+			print_error();
 			return (-1);
 		}
 		if (dup2(fd, 0) == -1)
 		{
+			print_error();
 			close(fd);
 			return (-1);
 		}
@@ -79,134 +83,114 @@ int		open_inputs(t_input *inputs, int test)
 // }
 
 //amiiiiiiines function
-int	open_outputs(t_output *outputs)
+int	open_outputs(t_output *outputs, int fd)
 {
 	int	new_fd;
 	int	tmp_fd;
 
 	if (outputs == NULL)
+	{
+		write(fd, "no output files\n", ft_strlen("no output files\n"));
 		return (0);
+	}
+	new_fd = 1;
 	while (outputs != NULL)
 	{
 		if (outputs->type == SIMPLE_REDIRECTION)
-			new_fd = open(outputs->file, O_WRONLY | O_CREAT, 00400);
+			new_fd = open(outputs->file, O_RDWR | O_CREAT, 0777);
 		else if (outputs->type == DOUBLE_REDIRECTION)
-			new_fd = open(outputs->file, O_WRONLY | O_APPEND | O_CREAT, 00400);
+			new_fd = open(outputs->file, O_RDWR | O_APPEND | O_CREAT, 0777);
 		if (new_fd < 0)
+		{
+			print_error();
 			return (-1);
+		}
 		tmp_fd = dup2(new_fd, 1);
 		if (tmp_fd < 0)
 		{
+			print_error();
 			close(new_fd);
 			return (-1);
 		}
 		outputs = outputs->next;
 	}
-	return (1);
+	return (new_fd);
 }
+
+int		dup_pipes(t_cmd *cmd, int *fd, int i)
+{
+	if (cmd->next != NULL)
+		return (dup2(fd[1], 1));
+	if (i != 0)
+		return (dup2(fd[0], 0));
+	return (0);
+}
+
 int		ft_exec_nested_cmd(t_cmd *cmd, char ***env)
 {
-	// pid_t	pid;
-	// t_cmd	*tmp;
-	// int		i;
-	// int		std_in = dup(0);
-	// int		std_out = dup(1);
-	// int		**pipes;
-	// int		cmd_size = size_cmds(cmd);
-	// int		out = 1;
-	// int		in = 0;
-	// char	buf[15];
-	// int inputs;
-	// int	outputs;
+	t_cmd	*tmp;
+	int		fd[2];
+	int		in;
+	int		out;
+	char	*path;
+	int		i;
+	pid_t	pid;
 
-	// tmp = cmd;
-	// pipes = get_pipes(cmd);
-	// i = 0;
-	// int test = open("test", O_WRONLY| O_CREAT | O_APPEND);
-	// char *path;
-	// int fd[2];
-	// int *adt = NULL;
-	// while (i < cmd_size)
-	// {
-	// 	pipe(pipes[i]);
-	// 	inputs = open_inputs(tmp->input, test);
-	// 	outputs = open_outputs(tmp->output);
-	// 	pid = fork();
-	// 	if (pid == 0)
-	// 	{
-	// 		if (inputs != 1)
-	// 		{
-	// 			write(test, "thers is no input file\n", ft_strlen("thers is no input file\n"));
-	// 			if (i != 0)
-	// 				in = pipes[i - 1][0];
-	// 			if (in != 0)
-	// 			{
-	// 				dup2(in, 0);
-	// 				close(in);
-	// 			}
-	// 		}
-	// 		if (outputs != 1)
-	// 		{
-	// 			write(test, "thers is no output file\n", ft_strlen("thers is no output file\n"));
-	// 			out = pipes[i][1];
-	// 			if (out != 1)
-	// 			{
-	// 				dup2(out, 1);
-	// 				close(out);
-	// 			}
-	// 		}
-	// 		path = command_path(tmp->name, *env);
-	// 		write(test, tmp->name, strlen(tmp->name));
-	// 		execve(path, tmp->args, *env);
-	// 		if (i + 1 == cmd_size)
-	// 		{
-	// 			dup2(std_out, 1);
-	// 			dup2(std_in, 0);
-	// 		}
-	// 		//close(fd[0]);
-	// 		//close(fd[1]);
-	// 		return (0);
-	// 	}
- 	// 	adt = &i;
-	// 	// 		read(pipes[i - 1][0], buf, 15);
-	// 	// 		write(1, buf, 15);
-	// 	// 		t++;
-	// 	// 	}
-	// 	// }
-	// 	i++;
-	// 	waitpid(pid, NULL, 0);
-	// 	tmp = tmp->next;
-	// }
+	tmp = cmd;
+	in = 0;
+	out = 1;
+	i = 0;
+	while (tmp != NULL)
+	{
+		pipe(fd);
+		pid = fork();
+		if (pid == 0)
+		{
+			if (dup_pipes(tmp, fd, 0) == -1)
+				print_error();
+			in = open_inputs(tmp->input, test_file);
+			out = open_outputs(tmp->output, test_file);
+			path = command_path(tmp->name, *env);
+			execve(path, tmp->args, *env);
+			return (0);
+		}
+		waitpid(pid, NULL, 0);
+		tmp = tmp->next;
+	}
 	return (0);
 }
 
 int main(int argc, char **argv, char **envp)
 {
 	t_cmd *cmd;
-	char **tab;
+	char **tap;
 
-	tab = ft_tabdup(envp);
+	test_file = open("test_file", O_RDWR |O_CREAT | O_APPEND, 0777);
+	tap = ft_tabdup(envp);
 	cmd	= (t_cmd *)malloc(sizeof(t_cmd));
-	cmd->name = strdup("cat");
-	cmd->args = ft_split("cat" ,32);
+	cmd->name = strdup("echo");
+	cmd->args = ft_split("echo test" ,32);
+	cmd->next = NULL;
+	cmd->input = NULL;
+	cmd->output = NULL;
+	cmd->option = NULL;
+	// cmd->input = (t_input *)malloc(sizeof(t_input));
+	// cmd->input->file = ft_strdup("file2");
+	// cmd->input->next = NULL;
 	
-	cmd->input = (t_input *)malloc(sizeof(t_input));
-	cmd->input->file = ft_strdup("file2");
-	cmd->input->next = NULL;
-	
-	cmd->output = NULL;//(t_output *)malloc(sizeof(t_output));
-	// cmd->output->file = ft_strdup("file3");
+	// cmd->output = (t_output *)malloc(sizeof(t_output));
+	// cmd->output->file = ft_strdup("file");
 	// cmd->output->type = SIMPLE_REDIRECTION;
 	// cmd->output->next = NULL;
 	
-	cmd->next = NULL;//(t_cmd *)malloc(sizeof(t_cmd));
-	// cmd->next->name = strdup("grep");
-	// cmd->next->args = ft_split("grep f", 32);
+	// cmd->next = (t_cmd *)malloc(sizeof(t_cmd));
+	// cmd->next->name = ft_strdup("grep");
+	// cmd->next->args = ft_split("grep t", 32);
 	
 	// cmd->next->input = (t_input *)malloc(sizeof(t_input));
 	// cmd->next->input->file = ft_strdup("file3");
 	// cmd->next->input->next = NULL;
 	
 	// cmd->next->next = NULL;
-	ft_exec_nested_cmd(cmd, &tab);
+	ft_exec_nested_cmd(cmd, &tap);
 }
