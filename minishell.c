0883ca30057b75085e910c1b9d7c2437904fb79d
mohamedamine456 <aminelachheb456@gmail.com>
2021-05-23 20:49:28 +0100

    ft_cd little prob

--- minishell.c
+++ minishell.c
@@ -6,7 +6,7 @@
 /*   By: mlachheb <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/29 15:03:49 by mlachheb          #+#    #+#             */
-/*   Updated: 2021/05/19 16:45:42 by mlachheb         ###   ########.fr       */
+/*   Updated: 2021/05/23 16:43:18 by mlachheb         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,8 +30,6 @@ int     main(int argc, char **argv, char **envp)
 			if (g_hist.command_line != NULL)
 			{
 				g_hist.retv = parse_execute(ft_strdup(g_hist.command_line), &new_envp, g_hist.retv);
-				//if (ft_strcmp(g_hist.command_line, ""))
-				//	write(1, "\n", 1);
 				write_history(&g_hist, ft_strdup(g_hist.command_line));
 				free(g_hist.command_line);
 			}
