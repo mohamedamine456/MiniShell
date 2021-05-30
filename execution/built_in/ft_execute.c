9caf54bc64d308b2e0c1e4894278fc72de0e71e1
El mehdi El orchi <eel-orch@e1r4p12.1337.ma>
2021-05-30 15:20:19 +0100

    fix segfault

--- execution/built_in/ft_execute.c
+++ execution/built_in/ft_execute.c
@@ -6,7 +6,7 @@
 /*   By: eel-orch <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/20 17:03:05 by eel-orch          #+#    #+#             */
-/*   Updated: 2021/05/29 20:41:13 by mlachheb         ###   ########.fr       */
+/*   Updated: 2021/05/30 15:17:22 by eel-orch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ int 	is_simple_cmd(t_cmd *cmd)
 
 int ft_execute(t_cmd *cmd, char ***envp)
 {
-	if (is_simple_cmd(cmd) == 0)
+	if (is_simple_cmd(cmd) == 1)
 		return (exec_builtin(cmd, envp));
 	ft_exec_nested_cmd(cmd, envp);
 	return (0);
