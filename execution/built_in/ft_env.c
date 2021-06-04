e479f79d0771b8d72e9b6ac8f89d21662ef079c0
mohamedamine456 <aminelachheb456@gmail.com>
2021-06-04 12:56:43 +0100

    commmit env backline from minishell

--- execution/built_in/ft_env.c
+++ execution/built_in/ft_env.c
@@ -6,7 +6,7 @@
 /*   By: mlachheb <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/10 08:35:02 by mlachheb          #+#    #+#             */
-/*   Updated: 2021/05/18 16:40:20 by mlachheb         ###   ########.fr       */
+/*   Updated: 2021/06/04 12:55:27 by mlachheb         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,11 +24,9 @@ void	ft_env(t_builtin_vars var, int *retv)
 		if (ft_isenv(envp[i]))
 		{
 			write(1, envp[i], ft_strlen(envp[i]));
-			if (envp[i + 1] != NULL)
-				write(1, "\n", 1);
+			write(1, "\n", 1);
 		}
 		i++;
 	}
 	*retv = 0;
-	write(1, "\n", 1);
 }
