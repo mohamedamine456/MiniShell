a99a289bcece1a6f5a2026f1a7613c5623ab9795
ELORCHI <elorchie@gmail.com>
2021-06-16 14:54:52 +0100

    rm trash files

--- main.c
+++ main.c
@@ -6,18 +6,24 @@
 /*   By: eel-orch <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/12 18:37:26 by eel-orch          #+#    #+#             */
-/*   Updated: 2021/06/12 18:43:02 by eel-orch         ###   ########.fr       */
+/*   Updated: 2021/06/16 13:40:23 by eel-orch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
+#include <errno.h>
+#include <stdio.h>
+#include <string.h>
 #include "libft/libft.h"
+#include <fcntl.h>
 
 int main()
 {
-	char *path = "/bin/ls";
-	char **args = ft_split("/bin/ls", '\0');
+	char *error_msg;
+	char *path = "/dfdsfsfd/dfdfd";
+	char **args = ft_split("dfdfd", 32);
 	execve(path, args, NULL);
-	write(2, "sdf", 3);
+	error_msg = strerror(errno);
+	write(2, error_msg, ft_strlen(error_msg));
 	return (0);
 }
