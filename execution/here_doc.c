031a1f71f962973aba52b1a4daebb7c29bdd370e
ELORCHI <elorchie@gmail.com>
2021-06-28 17:40:11 +0100

    here_doc norm and return value after exiting with ctrl c hanled

--- execution/here_doc.c
+++ execution/here_doc.c
@@ -6,7 +6,7 @@
 /*   By: eel-orch <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/26 15:41:19 by eel-orch          #+#    #+#             */
-/*   Updated: 2021/06/27 20:20:18 by mlachheb         ###   ########.fr       */
+/*   Updated: 2021/06/28 17:39:30 by eel-orch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,21 +23,33 @@ void	signals(void)
 		exit(1);
 }
 
-void close_and_exit(int fd)
+void	close_and_exit(int fd)
 {
 	close(fd);
 	exit(fd);
 }
 
+int	get_exit_status_and_exit(pid_t pid, int fd)
+{
+	int	exit_status;
+	int	status;
+
+	waitpid(pid, &status, 0);
+	exit_status = get_exit_status(status);
+	close(fd);
+	if (exit_status == 2)
+		return (1);
+	return (0);
+}
+
 int	here_doc(char *delimeter, char *file_name)
 {
-	int	fd;
-	char *line;
-	int	status;
-	pid_t pid;
+	int		fd;
+	char	*line;
+	int		status;
+	pid_t	pid;
 
 	fd = open(file_name, O_RDWR | O_TRUNC | O_CREAT, S_IWUSR | S_IRUSR);
-	//unlink(file_name);
 	pid = fork();
 	if (pid == 0)
 	{
@@ -56,8 +68,5 @@ int	here_doc(char *delimeter, char *file_name)
 		}
 		close_and_exit(fd);
 	}
-	waitpid(pid, &status, 0);
-	close(fd);
-	return (0);
+	return (get_exit_status_and_exit(pid, fd));
 }
-
