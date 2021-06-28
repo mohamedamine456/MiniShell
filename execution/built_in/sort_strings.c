72ddb0b3ee464faa294c7ee3d41d9275f59c3bd3
ELORCHI <elorchie@gmail.com>
2021-06-28 17:05:33 +0100

    norm fo ft_execute

--- execution/built_in/sort_strings.c
+++ execution/built_in/sort_strings.c
@@ -6,7 +6,7 @@
 /*   By: eel-orch <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/18 16:43:10 by eel-orch          #+#    #+#             */
-/*   Updated: 2021/04/18 17:00:56 by eel-orch         ###   ########.fr       */
+/*   Updated: 2021/06/28 17:05:01 by eel-orch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,10 @@
 
 void	sort_strings(char **table)
 {
-	char    *tmp;
-	int	    index;
-	int	    j;
-	int	    table_len;
+	char	*tmp;
+	int		index;
+	int		j;
+	int		table_len;
 
 	index = 1;
 	table_len = ft_strlen_tab(table);
@@ -34,19 +34,3 @@ void	sort_strings(char **table)
 		index++;
 	}
 }
-
-// int main()
-// {
-// 	char **tab = ft_split("kk Aaaa Caba AAAAA", 32);
-// 	sort_strings(tab);
-// 	int i = 0;
-// 	// while (tab[i])
-// 	// {
-// 	// 	printf("%s\n", tab[i]);
-// 	// 	free(tab[i]);
-// 	// 	i++;
-// 	// }
-// 	free(tab);
-// 	return (0);
-// }
-
