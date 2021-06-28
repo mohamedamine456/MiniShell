6a09b3d0ecc029cf0f2f40df4063949e5cb33eeb
ELORCHI <elorchie@gmail.com>
2021-06-28 17:03:13 +0100

    norm for is_built_in.c

--- execution/built_in/isbuilt_in.c
+++ execution/built_in/isbuilt_in.c
@@ -6,46 +6,44 @@
 /*   By: eel-orch <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/27 12:30:21 by eel-orch          #+#    #+#             */
-/*   Updated: 2021/04/27 12:30:23 by eel-orch         ###   ########.fr       */
+/*   Updated: 2021/06/28 17:00:17 by eel-orch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "built_in.h"
 
-int isbuilt_in(char *cmd)
+void	tolow(char *str)
 {
-	int		i;
-	int 	index;
-	char	*str;
+	int	index;
 
 	index = 0;
-	str = ft_strdup(cmd);
-	i = 0;
 	while (str[index])
 	{
-		if(str[index] >= 'A' && str[index] <= 'Z')
+		if (str[index] >= 'A' && str[index] <= 'Z')
 			str[index] = str[index] + 32;
 		index++;
 	}
-	if ((i = ft_strcmp(str, "echo")) == 0)
-		return (B_ECHO);
-	else if ((i = ft_strcmp(str, "export")) == 0)
-		return (B_EXPORT);
-	else if ((i = ft_strcmp(str, "env")) == 0)
-		return (B_ENV);
-	else if ((i = ft_strcmp(str, "exit")) == 0)
-		return (B_EXIT);
-	else if ((i = ft_strcmp(str, "cd")) == 0)
-		return (B_CD);
-	else if ((i = ft_strcmp(str, "unset")) == 0)
-		return (B_UNSET);
-	else if ((i = ft_strcmp(str, "pwd")) == 0)
-		return (B_PWD); 
-	return (0);
 }
 
-// int main()
-// {
-// 	char *str = "Echo";
-// 	isbuilt_in(str);
-// }
+int	isbuilt_in(char *cmd)
+{
+	int		i;
+	char	*str;
+
+	str = ft_strdup(cmd);
+	if (ft_strcmp(str, "echo") == 0)
+		return (B_ECHO);
+	else if (ft_strcmp(str, "export") == 0)
+		return (B_EXPORT);
+	else if (ft_strcmp(str, "env") == 0)
+		return (B_ENV);
+	else if (ft_strcmp(str, "exit") == 0)
+		return (B_EXIT);
+	else if (ft_strcmp(str, "cd") == 0)
+		return (B_CD);
+	else if (ft_strcmp(str, "unset") == 0)
+		return (B_UNSET);
+	else if (ft_strcmp(str, "pwd") == 0)
+		return (B_PWD);
+	return (0);
+}
