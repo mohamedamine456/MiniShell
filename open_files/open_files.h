3938d497bbcdeebeefe32b957cb1bc2d0919b73f
ELORCHI <elorchie@gmail.com>
2021-06-30 20:10:45 +0100

    norm for open_files

--- open_files/open_files.h
+++ open_files/open_files.h
@@ -1,7 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   open_files.h                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: eel-orch <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/06/30 19:59:43 by eel-orch          #+#    #+#             */
+/*   Updated: 2021/06/30 20:00:00 by eel-orch         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef OPEN_FILES_H
 # define OPEN_FILES_H
-#include "../minishell.h"
 
-int		open_redirections(t_redirection *redirection);
+# include "../minishell.h"
+
+int	open_redirections(t_redirection *redirection);
 
 #endif
--- open_files/open_outputs.c
+++ open_files/open_outputs.c
@@ -6,13 +6,13 @@
 /*   By: eel-orch <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/20 19:50:20 by eel-orch          #+#    #+#             */
-/*   Updated: 2021/06/27 20:10:55 by eel-orch         ###   ########.fr       */
+/*   Updated: 2021/06/30 20:10:12 by eel-orch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "open_files.h"
 
-int open_outputs_errors(char *str)
+int	open_outputs_errors(char *str)
 {
 	write(2, "minishell :", 11);
 	write(2, str, ft_strlen(str));
@@ -22,12 +22,41 @@ int open_outputs_errors(char *str)
 	return (-1);
 }
 
-int	open_redirections(t_redirection *redirection)
+int	open_files(t_redirection *tmp_redirec)
 {
 	int	new_fd;
+
+	new_fd = -1;
+	if (tmp_redirec->type == TRUNC)
+		new_fd = open(tmp_redirec->file,
+				O_WRONLY | O_CREAT | O_TRUNC, S_IWUSR | S_IRUSR);
+	else if (tmp_redirec->type == APPEND)
+		new_fd = open(tmp_redirec->file,
+				O_WRONLY | O_CREAT | O_APPEND, S_IWUSR | S_IRUSR);
+	else if (tmp_redirec->type == INPUT)
+		new_fd = open(tmp_redirec->file, O_RDONLY, S_IRUSR);
+	return (new_fd);
+}
+
+void	check_dup_errors(t_redirection *tmp_redirec, int new_fd)
+{
 	int	tmp_fd;
-	int error;
-	t_redirection *tmp_redirec;
+	int	error;
+
+	if (tmp_redirec->type == TRUNC || tmp_redirec->type == APPEND)
+		tmp_fd = dup2(new_fd, 1);
+	else
+		tmp_fd = dup2(new_fd, 0);
+	close(new_fd);
+	if (tmp_fd < 0)
+		error = open_outputs_errors(tmp_redirec->file);
+}
+
+int	open_redirections(t_redirection *redirection)
+{
+	int				new_fd;
+	int				error;
+	t_redirection	*tmp_redirec;
 
 	if (redirection == NULL)
 		return (0);
@@ -36,25 +65,14 @@ int	open_redirections(t_redirection *redirection)
 	error = 0;
 	while (tmp_redirec != NULL)
 	{
-		if (tmp_redirec->type == TRUNC)
-			new_fd = open(tmp_redirec->file, O_WRONLY | O_CREAT | O_TRUNC, S_IWUSR | S_IRUSR);
-		else if (tmp_redirec->type == APPEND)
-			new_fd = open(tmp_redirec->file, O_WRONLY | O_CREAT | O_APPEND, S_IWUSR | S_IRUSR);
-		else if (tmp_redirec->type == INPUT)
-			new_fd = open(tmp_redirec->file, O_RDONLY, S_IRUSR);
+		new_fd = open_files(tmp_redirec);
 		if (new_fd == -1)
 		{
 			error = open_outputs_errors(tmp_redirec->file);
 			tmp_redirec = tmp_redirec->next;
 			continue ;
 		}
-		if (tmp_redirec->type == TRUNC || tmp_redirec->type == APPEND)
-			tmp_fd = dup2(new_fd, 1);
-		else
-			tmp_fd = dup2(new_fd, 0);
-		close(new_fd);
-		if (tmp_fd < 0)
-			error = open_outputs_errors(tmp_redirec->file);
+		check_dup_errors(tmp_redirec, new_fd);
 		tmp_redirec = tmp_redirec->next;
 	}
 	return (error);
